5af472cb7dc09b29a5b7e7d0f8c95c38eab158a0
Frances Lin <franceslinyc@gmail.com>
2026-04-04 14:30:26 -0700

    LC 178 >1 Week Attempt

--- 178.rank-scores.py
+++ 178.rank-scores.py
@@ -6,7 +6,15 @@ import pandas as pd
 def order_scores(scores: pd.DataFrame) -> pd.DataFrame:
 
     scores['rank'] = scores['score'].rank(method = 'dense', ascending = False)
+                                         # method = 'dense': Rank with no gap
     
-    return scores[['score', 'rank']].sort_values('score', ascending = False)
+    return scores[['score', 'rank']].sort_values('score', ascending = False) 
 
-    # Careful! scores.sort_values('score', ascending=False) will return entire dataframe 
+            # Careful! Filter first, then sort. 
+            # scores[['score', 'rank']]: Filter columns
+            # .sort_values('score', ascending = False): Sort based on 'score' 
+            # Or else, scores.sort_values('score', ascending = False) return entire DataFrame
+            # Careful! [[]] return DataFrame. [] return Series. 
+
+
+                 
