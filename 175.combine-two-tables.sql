e4303a46159391e37d8526abb11d4f257d11d0d2
Frances Lin <franceslinyc@gmail.com>
2026-04-18 13:43:30 -0700

    Remove .sql files

--- 175.combine-two-tables.sql
+++
@@ -1,8 +0,0 @@
--- 175. Combine Two Tables
--- https://leetcode.com/problems/combine-two-tables/description/
-
-SELECT FirstName, LastName, City, State
-FROM Person 
-LEFT JOIN Address
-    ON Person.PersonId = Address.PersonId
-;
